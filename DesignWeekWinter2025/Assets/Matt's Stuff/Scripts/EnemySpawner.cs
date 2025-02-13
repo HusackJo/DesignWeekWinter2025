0302d835efe98e17141aefb038e543dc8c12fa74
mbarrameda <161363276+mbarrameda@users.noreply.github.com>
2025-02-12 22:18:15 -0500

    updated enemySpawn script

--- DesignWeekWinter2025/Assets/Matt's Stuff/Scripts/EnemySpawner.cs
+++ DesignWeekWinter2025/Assets/Matt's Stuff/Scripts/EnemySpawner.cs
@@ -8,12 +8,17 @@ public class EnemySpawner : MonoBehaviour
     private GameObject _enemyPrefab;
 
     [SerializeField]
-    private float _minimumSpawnTime;
+    private float _minimumSpawnTime = 3f;
+    [SerializeField]
+    private float _maximumSpawnTime = 6f;
 
     [SerializeField]
-    private float _maximumSpawnTime;
+    private float _spawnDecreaseRate = 0.2f;
+    [SerializeField]
+    private float _minSpawnLimit = 0.5f;
 
     private float _timeUntilSpawn;
+    private float _spawnIncreaseTimer = 0f;
 
     void Awake()
     {
@@ -24,17 +29,35 @@ public class EnemySpawner : MonoBehaviour
     void Update()
     {
         _timeUntilSpawn -= Time.deltaTime;
+        _spawnIncreaseTimer += Time.deltaTime;
 
         if (_timeUntilSpawn <= 0)
         {
             Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
             SetTimeUntilSpawn();
         }
+
+        if (_spawnIncreaseTimer > 5f)
+        {
+            DecreaseSpawnRate();
+            _spawnIncreaseTimer = 0f;
+        }
     }
 
     private void SetTimeUntilSpawn()
     {
         _timeUntilSpawn = Random.Range(_minimumSpawnTime, _maximumSpawnTime);
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+    }
+
+    private void DecreaseSpawnRate()
+    {
+        _minimumSpawnTime = Mathf.Max(_minimumSpawnTime - _spawnDecreaseRate, _minSpawnLimit);            // keeps spawn time above min limit but reduce gradually
+        _maximumSpawnTime = Mathf.Max(_maximumSpawnTime - _spawnDecreaseRate, _minSpawnLimit + 1f);
     }
 
 }
